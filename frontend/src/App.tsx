40ef64cb75e080018510538961583ca8987dbfb7
Gideon-Buba <bubaambore@gmail.com>
2025-04-03 13:28:21 +0100

    running tasks from frontnend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import Register from "./components/Register";
+import UserProfile from "./components/UserProfile";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>User Management</h1>
+      <Register />
+      <UserProfile />
+    </div>
+  );
 }
 
 export default App;
